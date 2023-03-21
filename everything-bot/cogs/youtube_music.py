b9b7203316ce9ab6bf899474dd0e7cb03648ed90
dandr94 <d.andreev1994@gmail.com>
2023-03-21 20:55:08 +0200

    Fixed all functionallity and added some documentation.

--- everything-bot/cogs/youtube_music.py
+++ everything-bot/cogs/youtube_music.py
@@ -1,21 +1,14 @@
 import datetime
-import pprint
 from collections import deque
-
 import discord
 import asyncio
-from discord.ui import Button
 from discord.ext import commands
 from yt_dlp import YoutubeDL
-
-YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True', 'quiet': True}
-FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-FFMPEG_PATH = {'linux': 'ffmpeg'}
+from cogs.utils import YouTubeMusicUtils
 
 
 class Queue:
     def __init__(self):
-        self.voice_channel = None
         self.queue = deque()
         self.playing_now = None
         self.playing_now_embed = None
@@ -51,100 +44,165 @@ class Queue:
     def get_by_id(self, id):
         return self.queue[id]
 
+    @staticmethod
+    def get_title(video):
+        return video.get('title')
 
-class YoutubeMusic(commands.Cog):
+    @staticmethod
+    def get_duration(video):
+        return video.get('duration')
+
+    @staticmethod
+    def get_youtube_url(video):
+        return video.get('webpage_url')
+
+    @staticmethod
+    def get_thumbnail(video):
+        return video.get('thumbnail')
+
+
+class YouTubeMusic(commands.Cog, YouTubeMusicUtils):
+    """
+        A class used to represent an YouTubeMusic player.
+        Users can interact with the play using the various commands of the class.
+
+        Available commands:
+        -------
+        1. !play (song name / YouTube URL) - Available also with !p or !play_video
+        Connects the bot to the channel if it's not already in the channel and
+        plays the requested song if the Queue is empty, otherwise places the song in the Queue.
+
+        2. !skip - Available also with !skip_video
+        Skips the current playing song.
+
+        3. !queue - Available also with !q or !que
+        Shows the current songs in the Queue. If it's empty it show 'Queue is empty.'
+
+        4. !leave - Available also with !l or !leave_channel
+        Kicks the bot from the current voice channel.
+
+        5. !stop - Available also with !stop_video
+        Stops the bot from playing and clears the Queue.
+        The bot remains in the channel for further usage.
+
+        6. !pause - Available also with !pause_video
+        Pauses the current song.
+
+        7. !resume - Available also with !resume_video
+        Resumes the current paused song.
+
+    """
+
     def __init__(self, bot: commands.Bot):
-        self.bot = bot
-        self.queue = Queue()
+        self.bot: commands.Bot = bot
+        self.queue: Queue = Queue()
+        self.voice_channel = None
 
     async def connect(self, ctx):
+        """
+        Connects the bot to the voice channel that the user who sent the message is currently in.
+        """
         try:
             if not ctx.message.author.voice:
-                await ctx.send('Error connect')
+                await ctx.send(self.CONNECT_ERROR_MESSAGE)
 
             vc = ctx.message.author.voice.channel
-            self.queue.voice_channel = await vc.connect()
+            self.voice_channel = await vc.connect()
         except:
             pass
 
-    def extract(self, arg):
+    def extract_video(self, arg):
+        """
+        Method that extracts information about a YouTube video using the provided search query or video ID.
+        """
         video = None
 
-        with YoutubeDL(YDL_OPTIONS) as ytdl:
+        with YoutubeDL(self.YDL_OPTIONS) as ydl:
             try:
-                video = ytdl.extract_info("ytsearch:%s" % arg, download=False)['entries'][0]
+                video = ydl.extract_info(f"ytsearch:{arg}", download=False)['entries'][0]
             except:
-                video = ytdl.extract_info(arg, download=False)
+                video = ydl.extract_info(arg, download=False)
 
         return video
 
-    def return_url(self, ext):
-        with YoutubeDL(YDL_OPTIONS) as ytdl:
-            url = ext['url']
-            return url
+    @staticmethod
+    def return_url(video):
+        """
+        Method that returns the url of the audio source.
+        """
+        url = video['url']
+        return url
+
+    def construct_queue_embed(self, now_playing):
+        """
+        The method constructs an embed message containing a list of videos that are in the Queue to be displayed.
+        If it's empty returns 'Queue is empty.'
+        """
+        if now_playing is None or self.queue.is_empty():
+            embed = discord.Embed(title=self.EMBED_QUEUE_EMPTY_TITLE, colour=self.EMBED_QUEUE_TITLE_COLOR)
+            return embed
+
+        embed = discord.Embed(title=self.EMBED_QUEUE_TITLE, colour=self.EMBED_QUEUE_TITLE_COLOR)
+
+        for i in range(self.queue.length()):
+            field_name = f'{i + 1 if i > 0 else "Next"}.'
+            video = self.queue.get_by_id(i - 1)
+            field_value = self.queue.get_title(video)
+
+            embed.add_field(name=field_name, value=field_value, inline=False)
+
+        return embed
+
+    @staticmethod
+    def construct_message_embed(title, description, color):
+        """
+        Embed message factory method that constructs an embed.
+        """
+
+        embed = discord.Embed(title=title,
+                              description=description,
+                              colour=color)
+
+        return embed
 
     async def play(self, ctx, url, video):
-        self.queue.voice_channel.play(
-            discord.FFmpegPCMAudio(source=url, **FFMPEG_OPTIONS),
-            )
+        """
+        The method uses the "play" method of the "voice_channel" object to play audio from the provided "url".
+        The method then retrieves information about the video being played.
+        Next, the method constructs an embed message to display information about the video being played.
 
-        duration = video.get('duration')
+        """
+        self.voice_channel.play(
+            discord.FFmpegPCMAudio(source=url, **self.FFMPEG_OPTIONS),
+        )
 
-        embed = discord.Embed(title='Now Playing:',
-                              description=f'{video.get("title")}',
-                              colour=0xff2a2a) \
-            .add_field(name='⌛ Duration:', value=datetime.timedelta(seconds=duration), inline=False) \
-            .add_field(name='🙃 Requested by:', value=ctx.author.mention, inline=False) \
-            .add_field(name='URL:', value=video.get('webpage_url'), inline=False) \
-            .set_thumbnail(url=video.get('thumbnail'))
-        # enter_btn = Button(style=discord.ButtonStyle.red, label='Enter', emoji='🚪'),
-        # stop_btn = Button(style=discord.ButtonStyle.red, label='Stop', emoji='🛑'),
-        # p_c_btn = Button(style=discord.ButtonStyle.success, label='Pause / Continue', emoji='⏯️'),
-        # skip_btn = Button(style=discord.ButtonStyle.success, label='Skip', emoji='⏭️')
-        # embed.add_field(name='Enter', value=enter_btn, inline=True)
-        # embed.add_field(name='Stop', value=stop_btn, inline=True)
-        # embed.add_field(name='Pause / Continue', value=p_c_btn, inline=True)
-        # embed.add_field(name='Skip', value=skip_btn, inline=True)
+        duration = self.queue.get_duration(video)
+        title = self.queue.get_title(video)
+        youtube_url = self.queue.get_youtube_url(video)
+        thumbnail = self.queue.get_thumbnail(video)
 
-        first_message = await ctx.send(
-            embed=embed
-            )
+        embed = self.construct_message_embed(title=self.EMBED_MESSAGE_TITLE, description=title,
+                                             color=self.EMBED_MESSAGE_TITLE_COLOR) \
+            .add_field(name=self.EMBED_MESSAGE_DURATION_NAME, value=datetime.timedelta(seconds=duration)) \
+            .add_field(name=self.EMBED_MESSAGE_REQUESTED_BY_NAME, value=ctx.author.mention) \
+            .add_field(name=self.EMBED_MESSAGE_URL_NAME, value=youtube_url, inline=False) \
+            .set_thumbnail(url=thumbnail)
 
-        self.queue.playing_now_embed = first_message
+        embed_message = await ctx.send(embed=embed)
 
-        title = video.get('title')
+        self.queue.playing_now_embed = embed_message
+
         self.queue.set_playing_now(title)
 
-        while self.queue.voice_channel.is_playing:
-            response = await self.bot.wait_for('button_click', check=lambda message: message.author == ctx.author)
-
-            if response.component.label == 'Enter':
-                await self.queue.playing_now_embed.edit(embed=embed, compnents=[])
-                self.leave(ctx)
-                await response.respond(content='Bot has entered the voice channel.')
-            elif response.component.label == 'Stop':
-                await self.queue.playing_now_embed.edit(embed=embed, components=[
-                    Button(style=discord.ButtonStyle.red, label='Enter', emoji='🚪')])
-                self.stop(ctx)
-                await response.respond(content='🛑 Stopped')
-                return
-            elif response.component.label == 'Pause / Continue':
-                if self.queue.voice_channel.is_playing():
-                    self.queue.voice_channel.pause()
-                    await response.respond(content='⏯️ Paused')
-                elif self.queue.voice_channel.is_pause():
-                    self.queue.voice_channel.resume()
-                    await response.respond(content='⏯️ Resumed')
-                return
-            elif response.component.label == 'Skip':
-                await self.queue.playing_now_embed.edit(embed=embed, components=[])
-                await response.respond(content='⏭️ Skipped')
-                self.skip(ctx)
-
     def skip(self, ctx):
-        asyncio.run_coroutine_threadsafe(self.queue.playing_now_embed.edit(components=[]), self.bot.loop)
-        if self.queue.voice_channel.is_playing():
-            self.queue.voice_channel.pause()
+        """
+        Method that handles playing the next track in the queue.
+        If there is a song in Queue it retrieves it and plays it. Otherwise, returns "Queue is empty".
+        """
+
+        asyncio.run_coroutine_threadsafe(self.queue.playing_now_embed.edit(), self.bot.loop)
+        if self.voice_channel.is_playing():
+            self.voice_channel.pause()
         if not self.queue.is_empty():
             next_track = self.queue.play_next()
             url = self.return_url(next_track)
@@ -152,43 +210,61 @@ class YoutubeMusic(commands.Cog):
             if self.queue.playing_now_embed is not None:
                 self.queue.playing_now_embed = None
         else:
-            asyncio.run_coroutine_threadsafe(ctx.send("Queue is empty!", delete_after=3),
+            asyncio.run_coroutine_threadsafe(ctx.send(self.EMBED_QUEUE_EMPTY_TITLE, delete_after=3),
                                              self.bot.loop)
 
     def stop(self, ctx):
+        """
+        Method that stops the audio and clears the Queue.
+        The bot remains in the channel for further usage.
+        """
         self.queue.clear_queue()
-        if self.queue.voice_channel.is_playing():
-            self.queue.voice_channel.stop()
-        elif self.queue.voice_channel.is_paused():
-            self.queue.voice_channel.stop()
+        if self.voice_channel.is_playing():
+            self.voice_channel.stop()
+        elif self.voice_channel.is_paused():
+            self.voice_channel.stop()
 
         self.queue.playing_now_embed = None
 
     def leave(self, ctx):
+        """
+        Method that pauses the bot and disconnects it from the channel.
+        """
         self.pause(ctx)
-        asyncio.run_coroutine_threadsafe(self.queue.voice_channel.disconnect(), self.bot.loop)
+        asyncio.run_coroutine_threadsafe(self.voice_channel.disconnect(), self.bot.loop)
 
     def pause(self, ctx):
-        if not self.queue.voice_channel.is_paused():
-            self.queue.voice_channel.pause()
+        """
+        Method that pauses the current playing song if song is being played.
+        """
+        if not self.voice_channel.is_paused():
+            self.voice_channel.pause()
 
     def resume(self, ctx):
-        if not self.queue.voice_channel.is_playing():
-            self.queue.voice_channel.resume()
-        elif self.queue.voice_channel.is_playing():
-            asyncio.run_coroutine_threadsafe(ctx.send('Not paused!'), self.bot.loop)
+        """
+        Method that resumes the current paused song. Otherwise, returns 'Not paused.'
+        """
+        if not self.voice_channel.is_playing():
+            self.voice_channel.resume()
+        elif self.voice_channel.is_playing():
+            asyncio.run_coroutine_threadsafe(ctx.send(self.RESUME_ERROR_MESSAGE), self.bot.loop)
 
     @commands.command(aliases=['play', 'p'])
     async def play_video(self, ctx, *, arg):
+        """
+        Command that uses the 'play' method.
+        It tries to retrieve the song otherwise, returns 'Song not found.'
+        If the song is found and the Queue is empty it plays the song otherwise, adds the song to the Queue.
+        """
         await self.connect(ctx)
         try:
-            video = self.extract(arg)
+            video = self.extract_video(arg)
         except IndexError:
-            await ctx.send('Song not found.')
-            await self.queue.voice_channel.disconnect()
+            await ctx.send(self.EXTRACT_ERROR_MESSAGE)
+            await self.voice_channel.disconnect()
             return
 
-        if not self.queue.voice_channel.is_playing():
+        if not self.voice_channel.is_playing():
             url = self.return_url(video)
 
             await self.play(ctx, url, video)
@@ -196,53 +272,65 @@ class YoutubeMusic(commands.Cog):
             self.queue.add_track(video)
             await ctx.send(f'{video.get("title")} added to queue.')
 
+    # Commands
     @commands.command(aliases=['skip', 's'])
     async def skip_video(self, ctx):
+        """
+        Command that uses the 'skip' method.
+        If it is successful it returns 'Skipped'.
+        """
         try:
             self.skip(ctx)
-            await ctx.send('Skipped')
+            await ctx.send(self.SKIP_MESSAGE)
         except AttributeError:
             return
 
-    @commands.command(aliases=['queue', 'q'])
-    async def queue_embed(self, ctx):
-        now = self.queue.return_playing_now()
+    @commands.command(aliases=['queue', 'q', 'que'])
+    async def show_queue(self, ctx):
+        """
+        Command uses the 'construct_queue_embed' method.
+        Returns an embedded message.
+        """
+        now_playing = self.queue.return_playing_now()
 
-        if now is not None:
-            embed = discord.Embed(title='Songs in Que:', colour=0xf0cd4f)
-            embed.add_field(name='Now playing:', value=now, inline=False)
+        embed = self.construct_queue_embed(now_playing)
 
-            for i in range(self.queue.length()):
-                video = self.queue.get_by_id(i)
-                embed.add_field(name=i + 1, value=video.get('title'), inline=False)
-
-            await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(title='Songs in Que', colour=0xf0cd4f, description='Queue is empty.')
-            await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
     @commands.command(aliases=['leave', 'l'])
     async def leave_channel(self, ctx):
+        """
+        Command that uses the 'leave' method.
+        If it is successful it returns a message.
+        """
         try:
             self.leave(ctx)
-            await ctx.send('Bot has left the channel.')
+            await ctx.send(self.LEAVE_CHANNEL_MESSAGE)
         except AttributeError:
             return
 
     @commands.command(aliases=['stop'])
     async def stop_video(self, ctx):
+        """
+        Command that uses the 'stop' method.
+        If it is successful it returns a message.
+        """
         try:
             self.stop(ctx)
-            await ctx.send('Stopped')
+            await ctx.send(self.STOP_MESSAGE)
         except AttributeError:
             return
 
-    @commands.command(aliases=['pause'])
+    @commands.command(aliases=['pause'])  # Works
     async def pause_video(self, ctx):
+        """
+        Command that uses the 'pause' method.
+        Returns a message if it is successful.
+        """
         try:
-            if not self.queue.voice_channel.is_paused():
+            if not self.voice_channel.is_paused():
                 self.pause(ctx)
-                await ctx.send('Video is paused.')
+                await ctx.send(self.PAUSE_MESSAGE)
             else:
                 return
 
@@ -251,12 +339,17 @@ class YoutubeMusic(commands.Cog):
 
     @commands.command(aliases=['resume', 'r'])
     async def resume_video(self, ctx):
+        """
+        Command that uses the 'resume' method.
+        If it is successful it returns a message.
+
+        """
         try:
             self.resume(ctx)
-            await ctx.send('Resumed')
+            await ctx.send(self.RESUME_MESSAGE)
         except AttributeError:
             return
 
 
 async def setup(bot):
-    await bot.add_cog(YoutubeMusic(bot))
+    await bot.add_cog(YouTubeMusic(bot))
